bf6e7b367fa5d7ffa2ca0b4e9d06b1192df76a75
fieldnight <da.young.yun13@gmail.com>
2024-04-10 15:49:47 +0900

    feat: page index.js에 타입 적용

--- pages/board/[boardId]/edit/index.tsx
+++ pages/board/[boardId]/edit/index.tsx
@@ -1,6 +1,10 @@
-import BoardWrite from "@/src/components/units/board/write/BoardWrite.container";
+import BoardWrite from "../../../../src/components/units/board/write/BoardWrite.container";
 import { useRouter } from "next/router";
 import { gql, useQuery } from "@apollo/client";
+import {
+  IQuery,
+  IQueryFetchBoardArgs,
+} from "../../../../src/commons/types/generated/types";
 
 const FETCH_BOARD = gql`
   query fetchBoard($boardId: ID!) {
@@ -14,9 +18,11 @@ const FETCH_BOARD = gql`
 `;
 
 export default function BoardEditPage() {
-
   const router = useRouter();
-  const { data } = useQuery(FETCH_BOARD, {
+  if(!router || typeof router.query.boardId !== "string") return <></>
+
+
+  const { data } = useQuery<Pick<IQuery,"fetchBoard">,IQueryFetchBoardArgs>(FETCH_BOARD, {
     variables: { boardId: router.query.boardId },
   });
   return <BoardWrite isEdit={true} data={data} />;
